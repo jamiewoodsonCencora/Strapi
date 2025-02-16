e31c16721e00db0c8b88337eea85f29cfe94a108
jamiewoodsonCencora <jamie.woodson@cencora.com>
2025-02-15 21:00:32 -0600

    Update server.ts

--- config/server.ts
+++ config/server.ts
@@ -1,6 +1,7 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url:  env('PUBLIC_URL', 'strapi-dev-app-aae8hreqarb8cpg3.centralus-01.azurewebsites.net'),
   app: {
     keys: env.array('APP_KEYS'),
   },
